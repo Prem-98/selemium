98c9c994dac6e6a510d160d9e55172bb4c7f7058
prem <prem164262@gmail.com>
2023-11-24 22:11:32 +0530

    Dropdowns UI

--- src/Selenium/java/microsoftrewards.java
+++ src/Selenium/java/microsoftrewards.java
@@ -13,8 +13,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 class microsoftrewards {
     public static void main(String args[]) throws InterruptedException
     {
-//        System.setProperty("webdriver.edge.driver","C:\\My Folder\\Selenium\\Drivers\\edgedriver_win64\\msedgedriver.exe");
-        WebDriverManager.edgedriver().setup();
+        System.setProperty("webdriver.edge.driver","C:\\My Folder\\Selenium\\Drivers\\edgedriver_win64\\msedgedriver.exe");
+//        WebDriverManager.edgedriver().setup();
 
         EdgeOptions edgeoptions = new EdgeOptions();
         edgeoptions.addArguments("--remote-allow-origins=*");
@@ -27,11 +27,11 @@ class microsoftrewards {
         driver.get("https://www.bing.com/");
         Thread.sleep(2000);
         wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//span[contains(text(),'prem')]"))));
-        driver.findElement(By.id("sb_form_q")).sendKeys("philips");
+        driver.findElement(By.id("sb_form_q")).sendKeys("surf");
         driver.findElement(By.id("search_icon")).click();
         for(int i=1;i<=40;i++)
         {	driver.findElement(By.id("sb_form_q")).clear();
-            driver.findElement(By.id("sb_form_q")).sendKeys("philips "+i);
+            driver.findElement(By.id("sb_form_q")).sendKeys("surf "+i);
             driver.findElement(By.id("sb_form_go")).click();
 
         }
