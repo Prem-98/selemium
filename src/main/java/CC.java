e9479b10ed52af6ab88948008b100a3c45e3a85f
prem <prem164262@gmail.com>
2024-07-02 07:37:51 +0530

    java concepts - inheritance

--- src/main/java/CC.java
+++ src/main/java/CC.java
@@ -4,11 +4,11 @@ public class CC extends data {
 
     @Test
     public void getData(){
-    doThis();}
+    doThis();} // here we are able to use doThis method which is written in data class because we have extended CC class to data class
 
     @Test
     public void calculate(){
-      PC pc=new PC(4);  // here we are passing 4 as a variable and it will be passed into cuntructor in PC class ans will be used globally
+      PC pc=new PC(4);  // here we are passing 4 as a variable and it will be passed into constructor in PC class and will be used globally
         System.out.println(pc.increment());
         System.out.println(pc.decrement());
         System.out.println(pc.multiplyWith2()); // here multiplyWith2 method can be accessed from PC object only because we have inherited PC2 in PC class
